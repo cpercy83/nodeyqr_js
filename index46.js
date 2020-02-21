11c90319c5caa4e76bf53c16ca09f9c954d801d7
cpercy83 <clinton.percy@gmail.com>
2020-02-20 21:28:36 -0600

    updated: index46.js

--- index46.js
+++ index46.js
@@ -1,7 +1,20 @@
 let userAndPassword = "pepito2017,12345";
-let userName = userAndPassword.substr(0, 10);
+
+let userName = userAndPassword.substr(0, 10);//When we know the size of the UN
 userName = userName.italics();
-let password = userAndPassword.substr(11, 5);
+let password = userAndPassword.substr(11, 5);//When we know the size of the PW
 password = password.italics();
 
-console.log(`The user ${userName} has the password ${password}`);
+let separator = userAndPassword.indexOf(","); //Thinking dynamically, we need to know how to separate the UN and PW as they will not be constant
+if (separator != -1) {
+    userName1 = userAndPassword.slice(0,separator);
+    password1 = userAndPassword.slice(separator+1)
+
+    console.log(`The user ${userName1} has the password ${password1}`);
+
+} else { 
+    
+    console.log(`Cannot find username or password\.`)
+}
+
+
