88c28d4a2c0d689961f2bee9ac19f47d0eb788fc
cpercy83 <clinton.percy@gmail.com>
2020-02-25 18:40:49 -0600

    added: index51.js

---
+++ index51.js
@@ -0,0 +1,8 @@
+let userName = "pepe2017";
+let passWord = "12345";
+
+if (userName == "pepe2017", passWord == "12345") {
+    console.log(`Logged in user; show user home page`);
+} else {
+    console.log(`Please log in to continue`);
+}
