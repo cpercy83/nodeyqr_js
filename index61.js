67d197a07fb2a925865723048c2e16968c6dd57e
cpercy83 <clinton.percy@gmail.com>
2020-02-25 20:12:57 -0600

    added: indexes 61, 63

---
+++ index61.js
@@ -0,0 +1,29 @@
+let weekdayNumber = 14;
+let message = null;
+
+switch (weekdayNumber) {
+case 1:
+        message = "Happy Monday!";
+break;
+case 2:
+    message = "Happy Tuesday!";
+break;
+case 3:
+    message = "Happy Hump Day!";
+break;
+case 4:
+    message = "Happy Turdsday!";
+break;
+case 5:
+    message = "Happy Friyay!";
+break;
+case 6:
+    message = "Enjoy Your Saturday!";
+break;
+case 7:
+    message = "Enjoy Your Sunday!";
+break;
+default: message = "Come Back When You Know the Days of the Week!";
+}
+
+console.log(`${message}`);
---
+++ index63.js
@@ -0,0 +1,47 @@
+let year = 2020
+let champion = null
+
+switch (year) {
+case 2006:
+    champion = "Carolina Hurricanes";
+    break;
+case 2007:
+    champion = "Anaheim Mighty Ducks";
+    break;
+case 2008:
+    champion = "Detroit Red Wings";
+    break;
+case 2009:
+    champion = "Pittsburgh Penguins";
+    break;
+case 2010:
+    champion = "Chicago Blackhawks";
+    break;
+case 2011:
+    champion = "Boston Bruins";
+    break;
+case 2012:
+    champion = "LA Kings";
+    break;
+case 2013:
+    champion = "Chicago Blackhawks";
+    break;
+case 2014:
+    champion = "LA Kings";
+    break;
+case 2015:
+    champion = "Chicago Blackhawks";
+    break;
+case 2016:
+    champion = "Pittsburgh Penguins";
+    break;
+case 2017:
+    champion = "Pittsburgh Penguins";
+    break;
+default:
+    console.error("Please try a year between 2006 and 2017");
+    break;
+}
+
+if (champion)
+console.log(`The winner in ${year} was ${champion}`);
